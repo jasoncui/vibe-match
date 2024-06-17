fa9b653c78792b509eb56d226f319cbe6712866f
Jason Cui <jason.s.cui11@gmail.com>
2024-06-17 15:20:40 -0700

    update dashboard UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
 
 export default async function Index() {
   const supabase = createClient();
@@ -20,7 +17,7 @@ export default async function Index() {
 
   const token = session.session.provider_token;
 
-  const fetchTopTracks = async (token) => {
+  const fetchTopTracks = async (token: string) => {
     const response = await fetch("https://api.spotify.com/v1/me/top/tracks", {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -36,7 +33,7 @@ export default async function Index() {
     }
   };
 
-  const fetchTopArtists = async (token) => {
+  const fetchTopArtists = async (token: string) => {
     const response = await fetch("https://api.spotify.com/v1/me/top/artists", {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -64,99 +61,83 @@ export default async function Index() {
     topTracks.items.length;
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <div>Vibe Match</div>
-          {<AuthButton />}
-        </div>
-      </nav>
-      {user ? (
-        <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3 pb-20">
-          <main className="flex-1 flex flex-col gap-10">
-            <div className="bg-gray-50 p-6 rounded-lg shadow-md mb-10">
-              <h3 className="font-bold text-xl mb-2 text-black">Basic Score</h3>
-              <p className="text-gray-700 mb-4">
-                The basic score is the average popularity score of all your top
-                tracks.
-              </p>
-              <p className="text-2xl font-semibold text-black">
-                {basicScore.toFixed(2)}
-              </p>
+    <div className="container mx-auto p-6 max-w-5xl pb-20">
+      <header className="flex justify-between items-center py-4 border-b border-gray-300">
+        <h1 className="text-3xl font-bold">Vibe Match</h1>
+        <AuthButton />
+      </header>
+
+      <div className="bg-gray-100 p-6 rounded-lg shadow-lg mb-10 mt-8">
+        <h3 className="font-bold text-xl mb-2">Basic Score</h3>
+        <p className="text-gray-700 mb-4">
+          The basic score is the average popularity score of all your top
+          tracks.
+        </p>
+        <p className="text-2xl font-semibold">{basicScore.toFixed(2)}</p>
+      </div>
+
+      <div className="mb-8">
+        <h3 className="font-bold text-2xl mb-4">Your Top Tracks</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {topTracks.items.map((track, index) => (
+            <div
+              key={track.id}
+              className="bg-white p-4 rounded-lg shadow-md flex items-center"
+            >
+              <span className="text-lg text-gray-600 font-bold mr-4">
+                {index + 1}
+              </span>
+              <img
+                src={track.album.images[0].url}
+                alt={track.name}
+                className="w-16 h-16 rounded-lg mr-4"
+              />
+              <div>
+                <div className="text-black font-semibold">{track.name}</div>
+                <div className="text-gray-600">
+                  by {track.artists.map((artist) => artist.name).join(", ")}
+                </div>
+                <div className="text-sm text-gray-500 mt-1">
+                  Album: {track.album.name}
+                </div>
+                <div className="text-sm text-gray-500">
+                  Release Date: {track.album.release_date}
+                </div>
+              </div>
             </div>
-            <div className="mb-4">
-              <h3 className="font-bold text-2xl text-black">Your Top Tracks</h3>
-            </div>
-            <ul className="space-y-4">
-              {topTracks.items.map((track, index) => (
-                <li
-                  key={track.id}
-                  className="flex items-center space-x-4 p-4 rounded-lg"
-                >
-                  <span className="text-lg text-gray-600 font-bold">
-                    {index + 1}
-                  </span>
-                  <img
-                    src={track.album.images[0].url}
-                    alt={track.name}
-                    className="w-16 h-16 rounded-lg"
-                  />
-                  <div className="flex-1">
-                    <div className="text-black font-semibold">{track.name}</div>
-                    <div className="text-gray-600">
-                      by {track.artists.map((artist) => artist.name).join(", ")}
-                    </div>
-                    <div className="text-sm text-gray-500 mt-1">
-                      Album: {track.album.name}
-                    </div>
-                    <div className="text-sm text-gray-500">
-                      Release Date: {track.album.release_date}
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-            <div className="mt-10">
-              <h3 className="font-bold text-2xl text-black mb-4">
-                Your Top Artists
-              </h3>
-              <ul className="space-y-4">
-                {topArtists.items.map((artist, index) => (
-                  <li
-                    key={artist.id}
-                    className="flex items-center space-x-4 p-4 rounded-lg"
-                  >
-                    <span className="text-lg text-gray-600 font-bold">
-                      {index + 1}
-                    </span>
-                    <img
-                      src={artist.images[0]?.url}
-                      alt={artist.name}
-                      className="w-16 h-16 rounded-lg"
-                    />
-                    <div className="flex-1">
-                      <div className="text-black font-semibold">
-                        {artist.name}
-                      </div>
-                      <div className="text-gray-600">
-                        Followers: {artist.followers.total.toLocaleString()}
-                      </div>
-                      <div className="text-sm text-gray-500 mt-1">
-                        Genres: {artist.genres.join(", ")}
-                      </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </main>
+          ))}
         </div>
-      ) : (
-        <div>
-          Vibe match connects to your Spotify and sees how your music vibes
-          match up with your friends. You are not logged in.
+      </div>
+
+      <div>
+        <h3 className="font-bold text-2xl mb-4">Your Top Artists</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {topArtists.items.map((artist, index) => (
+            <div
+              key={artist.id}
+              className="bg-white p-4 rounded-lg shadow-md flex items-center"
+            >
+              <span className="text-lg text-gray-600 font-bold mr-4">
+                {index + 1}
+              </span>
+              <img
+                src={artist.images[0]?.url}
+                alt={artist.name}
+                className="w-16 h-16 rounded-lg mr-4"
+              />
+              <div>
+                <div className="text-black font-semibold">{artist.name}</div>
+                <div className="text-gray-600">
+                  Followers: {artist.followers.total.toLocaleString()}
+                </div>
+                <div className="text-sm text-gray-500 mt-1">
+                  Genres: {artist.genres.join(", ")}
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      )}
+      </div>
     </div>
   );
 }
