9ec9cd20a9261d3517e883df78bdebb0233213dd
Jason Cui <jason.s.cui11@gmail.com>
2024-06-17 09:48:13 -0700

    remove unneeded signin

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,32 +2,12 @@ import Link from "next/link";
 import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import { SubmitButton } from "./submit-button";
 
 export default function Login({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
-  const signIn = async (formData: FormData) => {
-    "use server";
-
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-    if (error) {
-      return redirect("/login?message=Could not authenticate user");
-    }
-
-    return redirect("/");
-  };
-
   const handleSignIn = async () => {
     "use server";
 
