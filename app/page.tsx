8159f3c49d08bd977e31a8540d9e3eca729cb4aa
Jason Cui <jason.s.cui11@gmail.com>
2024-06-18 15:48:05 -0700

    fix build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,6 @@
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import {
-  Key,
-  ReactElement,
-  JSXElementConstructor,
-  ReactNode,
-  ReactPortal,
-  PromiseLikeOfReactNode,
-} from "react";
 
 export default async function Index() {
   const supabase = createClient();
@@ -57,11 +49,50 @@ export default async function Index() {
     }
   };
 
-  // Fetch the top tracks from Spotify
+  // Fetch the top tracks and top artists from Spotify
   const topTracks = await fetchTopTracks(token);
-
-  // Fetch the top artists from Spotify
   const topArtists = await fetchTopArtists(token);
+
+  // Format tracks and artists data for Supabase
+  const tracksData = topTracks.items.map(
+    (track: {
+      id: any;
+      name: any;
+      album: { name: any; release_date: any; images: any };
+      artists: any[];
+      popularity: any;
+    }) => ({
+      id: track.id,
+      user_id: user?.id,
+      name: track.name,
+      album: track.album.name,
+      artists: track.artists.map((artist) => artist.name),
+      release_date: track.album.release_date,
+      popularity: track.popularity,
+      images: track.album.images,
+    })
+  );
+
+  const artistsData = topArtists.items.map(
+    (artist: {
+      id: any;
+      name: any;
+      genres: any;
+      followers: { total: any };
+      images: any;
+    }) => ({
+      id: artist.id,
+      user_id: user?.id,
+      name: artist.name,
+      genres: artist.genres,
+      followers: artist.followers.total,
+      images: artist.images,
+    })
+  );
+
+  // Store the fetched data into Supabase
+  // await storeDataInSupabase(user.id, tracksData, "tracks");
+  // await storeDataInSupabase(user.id, artistsData, "artists");
 
   // Calculate the basic score
   const basicScore =
@@ -89,80 +120,43 @@ export default async function Index() {
       <div className="mb-8">
         <h3 className="font-bold text-2xl mb-4">Your Top Tracks</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {topTracks.items.map(
-            (
-              track: {
-                id: Key | null | undefined;
-                album: {
-                  images: { url: string | undefined }[];
-                  name:
-                    | string
-                    | number
-                    | boolean
-                    | ReactElement<any, string | JSXElementConstructor<any>>
-                    | Iterable<ReactNode>
-                    | ReactPortal
-                    | PromiseLikeOfReactNode
-                    | null
-                    | undefined;
-                  release_date:
-                    | string
-                    | number
-                    | boolean
-                    | ReactElement<any, string | JSXElementConstructor<any>>
-                    | Iterable<ReactNode>
-                    | ReactPortal
-                    | PromiseLikeOfReactNode
-                    | null
-                    | undefined;
-                };
-                name:
-                  | string
-                  | number
-                  | boolean
-                  | ReactElement<any, string | JSXElementConstructor<any>>
-                  | Iterable<ReactNode>
-                  | PromiseLikeOfReactNode
-                  | null
-                  | undefined;
-                artists: any[];
-              },
-              index: number
-            ) => (
-              <div
-                key={track.id}
-                className="bg-white p-4 rounded-lg shadow-md flex items-center"
-              >
-                <span className="text-lg text-gray-600 font-bold mr-4">
-                  {index + 1}
-                </span>
-                <img
-                  src={track.album.images[0].url}
-                  alt={track.name}
-                  className="w-16 h-16 rounded-lg mr-4"
-                />
-                <div>
-                  <div className="text-black font-semibold">{track.name}</div>
-                  <div className="text-gray-600">
-                    by {track.artists.map((artist) => artist.name).join(", ")}
-                  </div>
-                  <div className="text-sm text-gray-500 mt-1">
-                    Album: {track.album.name}
-                  </div>
-                  <div className="text-sm text-gray-500">
-                    Release Date: {track.album.release_date}
-                  </div>
+          {topTracks.items.map((track: any, index: any) => (
+            <div
+              key={track.id}
+              className="bg-white p-4 rounded-lg shadow-md flex items-center"
+            >
+              <span className="text-lg text-gray-600 font-bold mr-4">
+                {index + 1}
+              </span>
+              <img
+                src={track.album.images[0].url}
+                alt={track.name}
+                className="w-16 h-16 rounded-lg mr-4"
+              />
+              <div>
+                <div className="text-black font-semibold">{track.name}</div>
+                <div className="text-gray-600">
+                  by{" "}
+                  {track.artists
+                    .map((artist: { name: any }) => artist.name)
+                    .join(", ")}
+                </div>
+                <div className="text-sm text-gray-500 mt-1">
+                  Album: {track.album.name}
+                </div>
+                <div className="text-sm text-gray-500">
+                  Release Date: {track.album.release_date}
                 </div>
               </div>
-            )
-          )}
+            </div>
+          ))}
         </div>
       </div>
 
       <div>
         <h3 className="font-bold text-2xl mb-4">Your Top Artists</h3>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {topArtists.items.map((artist, index) => (
+          {topArtists.items.map((artist: any, index: any) => (
             <div
               key={artist.id}
               className="bg-white p-4 rounded-lg shadow-md flex items-center"
